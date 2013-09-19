c255a6366a56bb4d2f9ae2bafb980e5f604ecf96
Ruth <rcitlax@gmail.com>
2013-09-18 21:39:20 -0300

    Subiendo Katas

---
+++ roman_number.rb
@@ -0,0 +1,22 @@
+class RomanNumber
+
+	def initialize(number)
+	
+	@value=number
+
+	end 
+
+	def value
+	return @value
+	
+	end
+
+	def add(number)
+	@value+=number
+	return @value
+	
+	end
+
+end
+
+
